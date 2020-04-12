eb47a78d2810209e30b343c89cac1a509c9b19fe
Christian <37517470+DraconicChris@users.noreply.github.com>
2020-04-12 17:44:04 -0400

    Update DialogueServerScript.lua

--- plugin/DialogueServerScript.lua
+++ plugin/DialogueServerScript.lua
@@ -2,6 +2,9 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local Players = game:GetService("Players");
 
+-- Get dialogue settings
+local Settings = require(script.Settings);
+
 -- Make sure that we have a connection with the remote functions/events
 local RemoteConnections = ReplicatedStorage:WaitForChild("DialogueMakerRemoteConnections",3);
 if not RemoteConnections then
@@ -20,4 +23,20 @@ end;
 
 RemoteConnections.GetNPCDialogue.OnServerInvoke = function(player)
 	return DialogueLocations;
+end
+
+RemoteConnections.GetDefaultTheme.OnServerInvoke = function(player)
+	return Settings.DefaultTheme;
+end
+
+RemoteConnections.GetAllThemes.OnServerInvoke = function(player)
+	
+	local Themes = {}
+	
+	for _, theme in ipairs(script.Themes:GetChildren()) do
+		table.insert(Themes, theme:Clone());
+	end;
+	
+	return Themes;
+	
 end
