9511009c4407023b4b86355c7b0dc2a2c7503e58
Christian <37517470+DraconicChris@users.noreply.github.com>
2020-04-12 20:48:52 -0400

    Update ConditionTemplate.lua

--- server/ConditionTemplate.lua
+++ server/ConditionTemplate.lua
@@ -1,7 +1,7 @@
 return function()
 	
 	-- In order for this condition to pass, it must return true.
-	-- Otherwise, lower priority dialogue or no dialogue will be used.
+	-- Otherwise, lower priority dialogue/responses or no dialogue/responses will be used.
 	return true;
 	
 end;
