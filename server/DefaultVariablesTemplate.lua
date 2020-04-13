7276c04bcb63610e51c5db2018befded70d7d9f2
Christian <37517470+DraconicChris@users.noreply.github.com>
2020-04-13 11:54:15 -0400

    Create DefaultVariablesTemplate.lua

---
+++ server/DefaultVariablesTemplate.lua
@@ -0,0 +1,7 @@
+-- These variables will be used when they are called in an NPC's dialogue
+-- by using [/variable="VARIABLE_NAME"]. 
+-- Replace VARIABLE_NAME with the variable you want.
+
+return {
+	VARIABLE_NAME = "Change this lol"
+};
