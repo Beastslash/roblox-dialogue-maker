a472386e5f6561d4bf13c89edf669d84bedd5f2f
Christian <37517470+DraconicChris@users.noreply.github.com>
2020-04-12 21:07:10 -0400

    Update DialoguePluginScript.lua

--- plugin/DialoguePluginScript.lua
+++ plugin/DialoguePluginScript.lua
@@ -116,7 +116,7 @@ local function SyncDialogueGui(directory)
 			for _, child in ipairs(ServerScriptService.DialogueServerScript.Conditions:GetChildren()) do
 				
 				-- Check if the child is a condition
-				if child:IsA("ModuleScript") and child.Priority.Value == dialogue.Priority.Value and child.NPC.Value == Model then
+				if child:IsA("ModuleScript") and child.Priority.Value == tostring(dialogue.Priority.Value) and child.NPC.Value == Model then
 					
 					-- Return the condiiton
 					Condition = child;
