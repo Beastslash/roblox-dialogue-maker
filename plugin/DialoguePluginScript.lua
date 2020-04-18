07672fe1191fd1b98a61eda1bb89e7b87015b519
Christian <37517470+DraconicChris@users.noreply.github.com>
2020-04-17 23:29:25 -0400

    Replace settings GUI with a ModuleScript
    Closes #11

--- plugin/DialoguePluginScript.lua
+++ plugin/DialoguePluginScript.lua
@@ -13,8 +13,6 @@ local PluginGui;
 local DialogueMakerFrame = script.DialogueMakerGUI.MainFrame:Clone();
 local DialogueMessageList = DialogueMakerFrame.DialogueContainer.DialogueMessageList;
 local DialogueMessageTemplate = DialogueMessageList.DialogueMessageTemplate:Clone();
-local SettingsFrame = DialogueMakerFrame.SettingsFrame;
-local PartSelectionFrame = DialogueMakerFrame.PartSelection;
 local Tools = DialogueMakerFrame.Tools;
 local Events = {EditingMessage = {}; EditingRedirect = {}; ConvertFromRedirect = {}; ConvertToRedirect = {}};
 
@@ -38,12 +36,12 @@ local function CloseDialogueEditor()
 		Events[key] = nil;
 	end;
 	
+	Events = {EditingMessage = {}; EditingRedirect = {}; ConvertFromRedirect = {}; ConvertToRedirect = {}};
+	
 	ViewingPriority = "1";
 	DialogueMakerFrame = DialogueMakerFrame:Clone();
 	DialogueMessageList = DialogueMakerFrame.DialogueContainer.DialogueMessageList;
 	DialogueMessageTemplate = DialogueMessageTemplate:Clone();
-	SettingsFrame = DialogueMakerFrame.SettingsFrame;
-	PartSelectionFrame = DialogueMakerFrame.PartSelection;
 	Tools = DialogueMakerFrame.Tools;
 	
 	PluginGui:Destroy();
@@ -55,58 +53,23 @@ end;
 local DeleteModeEnabled = false;
 local DeletePromptShown = false;
 
-local function SyncDialogueGui(directoryDialogue)
+local function RepairNPC()
 	
-	-- Reset the current dialogue container
-	CurrentDialogueContainer = Model:FindFirstChild("DialogueContainer");
-	if not CurrentDialogueContainer then
-		warn("[Dialogue Maker] "..Model.Name.."'s DialogueContainer was moved or deleted! Creating a new one...");
+	if not Model:FindFirstChild("DialogueContainer") then
+		
+		if new then
+			print("[Dialogue Maker] Created a DialogueContainer inside of "..Model.Name)
+		else
+			warn("[Dialogue Maker] "..Model.Name.."'s DialogueContainer was moved or deleted! Creating a new one...");
+		end
 		
 		-- Add the dialogue container to the NPC
 		CurrentDialogueContainer = Instance.new("Folder");
 		CurrentDialogueContainer.Name = "DialogueContainer";
 		
-		-- Add configuration to the container
-		local DialogueSettings = Instance.new("Configuration");
-		DialogueSettings.Name = "Settings";
-		DialogueSettings.Parent = CurrentDialogueContainer;
-		
-		-- Add flags to the configuration
-		local PromptRegionEnabled = Instance.new("BoolValue");
-		PromptRegionEnabled.Name = "PromptRegionEnabled";
-		PromptRegionEnabled.Parent = DialogueSettings;
-		
-		local PromptRadius = Instance.new("ObjectValue");
-		PromptRadius.Name = "PromptRegionPart";
-		PromptRadius.Parent = DialogueSettings;
-		
-		local TimeoutEnabled = Instance.new("BoolValue");
-		TimeoutEnabled.Name = "TimeoutEnabled";
-		TimeoutEnabled.Parent = DialogueSettings;
-		
-		local Timeout = Instance.new("IntValue");
-		Timeout.Name = "TimeoutInSeconds";
-		Timeout.Parent = DialogueSettings;
-		
-		local SpeechBubbleEnabled = Instance.new("BoolValue");
-		SpeechBubbleEnabled.Name = "SpeechBubbleEnabled";
-		SpeechBubbleEnabled.Parent = DialogueSettings;
-		
-		local SpeechBubblePart = Instance.new("ObjectValue");
-		SpeechBubblePart.Name = "SpeechBubblePart";
-		SpeechBubblePart.Parent = DialogueSettings;
-		
-		local ClickEnabled = Instance.new("BoolValue");
-		ClickEnabled.Name = "ClickEnabled";
-		ClickEnabled.Parent = DialogueSettings;
-		
-		local ClickDetectorLocation = Instance.new("ObjectValue");
-		ClickDetectorLocation.Name = "ClickDetectorLocation";
-		ClickDetectorLocation.Parent = DialogueSettings;
-		
-		local Theme = Instance.new("StringValue");
-		Theme.Name = "Theme";
-		Theme.Parent = DialogueSettings;
+		local SettingsScript = script.NPCSettingsTemplate:Clone();
+		SettingsScript.Name = "Settings";
+		SettingsScript.Parent = CurrentDialogueContainer;
 		
 		-- Create a root folder
 		local TempRootFolder = Instance.new("Folder");
@@ -132,9 +95,32 @@ local function SyncDialogueGui(directoryDialogue)
 		CurrentDialogueContainer.Parent = Model;
 		
 		ViewingPriority = "1";
-		SyncDialogueGui(CurrentDialogueContainer["1"].Dialogue);
+		
+		-- There's nothing else we can do to repair the scripts; we can't find em!
 		return;
+		
 	end;
+	
+	CurrentDialogueContainer = Model:FindFirstChild("DialogueContainer");
+	
+	if not CurrentDialogueContainer:FindFirstChild("Settings") then
+		
+		print("[Dialogue Maker] Adding settings script to "..Model.Name)
+		
+		local SettingsScript = script.NPCSettingsTemplate:Clone();
+		SettingsScript.Name = "Settings";
+		SettingsScript.Parent = CurrentDialogueContainer;
+		
+		print("[Dialogue Maker] Added settings script to "..Model.Name)
+		
+	end;
+	
+end;
+
+local function SyncDialogueGui(directoryDialogue)
+	
+	-- Make sure everything with the NPC is OK
+	RepairNPC();
 	
 	if ViewingPriority == "1" then
 		DialogueMakerFrame.ViewStatus.DialogueLocationStatus.Text = "Viewing the beginning of the conversation";
@@ -733,324 +719,12 @@ local function OpenDialogueEditor()
 	PluginGui.Title = "Dialogue Maker";
 	PluginGui:BindToClose(CloseDialogueEditor);
 	
-	local SettingsOpen = false;
-	
 	Events.AdjustSettingsRequested = Tools.AdjustSettings.MouseButton1Click:Connect(function()
 		
-		-- Make sure the user isn't spamming the button 
-		if SettingsOpen then
-			return;
-		end;
-		SettingsOpen = true;
+		-- Make sure all of the important objects are in the NPC
+		RepairNPC();
 		
-		-- Let's enable the buttons based on the settings we got.
-		-- Check if a prompt radius enabled
-		local function EnablePromptRegion(sync)
-			SettingsFrame.PromptRegionEnabled.BackgroundColor3 = Color3.fromRGB(73,195,81);
-			SettingsFrame.PromptRegionEnabled.Text = "Prompt region enabled";
-			SettingsFrame.DefinePromptRegion.BackgroundColor3 = Color3.fromRGB(122,122,122);
-			SettingsFrame.DefinePromptRegion.TextColor3 = Color3.fromRGB(27,27,27);
-			
-			if sync then
-				CurrentDialogueContainer.Settings.PromptRegionEnabled.Value = true;
-			end;
-		end;
-		
-		local function DisablePromptRegion(sync)
-			SettingsFrame.PromptRegionEnabled.BackgroundColor3 = Color3.fromRGB(255,115,115);
-			SettingsFrame.PromptRegionEnabled.Text = "Prompt region disabled";
-			SettingsFrame.DefinePromptRegion.BackgroundColor3 = Color3.fromRGB(81,81,81);
-			SettingsFrame.DefinePromptRegion.TextColor3 = Color3.fromRGB(27,27,27);
-			
-			if sync then
-				CurrentDialogueContainer.Settings.PromptRegionEnabled.Value = false;
-			end;
-		end;
-		
-		local function EnableConversationTimeout(sync)
-			SettingsFrame.TimeoutEnabled.BackgroundColor3 = Color3.fromRGB(73,195,81);
-			SettingsFrame.TimeoutEnabled.Text = "Conversation timeout enabled";
-			SettingsFrame.TimeoutInSeconds.BackgroundColor3 = Color3.fromRGB(122,122,122);
-			SettingsFrame.TimeoutInSeconds.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.TimeoutInSeconds.TextEditable = true;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.TimeoutEnabled.Value = true;
-			end;
-		end;
-		
-		local function DisableConversationTimeout(sync)
-			SettingsFrame.TimeoutEnabled.BackgroundColor3 = Color3.fromRGB(255,115,115);
-			SettingsFrame.TimeoutEnabled.Text = "Conversation timeout disabled";
-			SettingsFrame.TimeoutInSeconds.BackgroundColor3 = Color3.fromRGB(81,81,81);
-			SettingsFrame.TimeoutInSeconds.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.TimeoutInSeconds.TextEditable = false;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.TimeoutEnabled.Value = false;
-			end;
-		end;
-		
-		local function EnableSpeechBubble(sync)
-			SettingsFrame.SpeechBubbleEnabled.BackgroundColor3 = Color3.fromRGB(73,195,81);
-			SettingsFrame.SpeechBubbleEnabled.Text = "Speech bubble enabled";
-			SettingsFrame.DefineSpeechBubblePart.BackgroundColor3 = Color3.fromRGB(122,122,122);
-			SettingsFrame.DefineSpeechBubblePart.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.DefineSpeechBubblePart.AutoButtonColor = true;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.SpeechBubbleEnabled.Value = true;
-			end;
-		end;
-		
-		local function DisableSpeechBubble(sync)
-			SettingsFrame.SpeechBubbleEnabled.BackgroundColor3 = Color3.fromRGB(255,115,115);
-			SettingsFrame.SpeechBubbleEnabled.Text = "Speech bubble disabled";
-			SettingsFrame.DefineSpeechBubblePart.BackgroundColor3 = Color3.fromRGB(81,81,81);
-			SettingsFrame.DefineSpeechBubblePart.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.DefineSpeechBubblePart.AutoButtonColor = false;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.SpeechBubbleEnabled.Value = false;
-			end;
-		end;
-		
-		local function EnableClick(sync)
-			SettingsFrame.ClickEnabled.BackgroundColor3 = Color3.fromRGB(73,195,81);
-			SettingsFrame.ClickEnabled.Text = "Clicking enabled";
-			SettingsFrame.DefineClickDetector.BackgroundColor3 = Color3.fromRGB(122,122,122);
-			SettingsFrame.DefineClickDetector.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.DefineClickDetector.AutoButtonColor = true;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.ClickEnabled.Value = true;
-			end;
-		end;
-		
-		local function DisableClick(sync)
-			SettingsFrame.ClickEnabled.BackgroundColor3 = Color3.fromRGB(255,115,115);
-			SettingsFrame.ClickEnabled.Text = "Clicking disabled";
-			SettingsFrame.DefineClickDetector.BackgroundColor3 = Color3.fromRGB(81,81,81);
-			SettingsFrame.DefineClickDetector.TextColor3 = Color3.fromRGB(27,27,27);
-			SettingsFrame.DefineClickDetector.AutoButtonColor = false;
-			
-			if sync then
-				CurrentDialogueContainer.Settings.ClickEnabled.Value = false;
-			end;
-		end;
-		
-		local Busy = false;
-		
-		-- Now let's sync the dialogue settings with the settings GUI
-		if CurrentDialogueContainer.Settings.PromptRegionEnabled.Value then
-			EnablePromptRegion(false);
-		end;
-		
-		if CurrentDialogueContainer.Settings.TimeoutEnabled.Value then
-			EnableConversationTimeout(false);
-		end;
-		
-		if CurrentDialogueContainer.Settings.SpeechBubbleEnabled.Value then
-			EnableSpeechBubble(false);
-		end;
-		
-		if CurrentDialogueContainer.Settings.ClickEnabled.Value then
-			EnableClick(false);
-		end;
-		
-		-- Now let's add the option to toggle those buttons
-		SettingsFrame.PromptRegionEnabled.MouseButton1Click:Connect(function()
-			if CurrentDialogueContainer.Settings.PromptRegionEnabled.Value then
-				DisablePromptRegion(true);
-			else
-				EnablePromptRegion(true);
-			end;
-		end);
-			
-		SettingsFrame.TimeoutEnabled.MouseButton1Click:Connect(function()
-			if CurrentDialogueContainer.Settings.TimeoutEnabled.Value then
-				DisableConversationTimeout(true);
-			else
-				EnableConversationTimeout(true);
-			end;
-		end);
-			
-		SettingsFrame.SpeechBubbleEnabled.MouseButton1Click:Connect(function()
-			if CurrentDialogueContainer.Settings.SpeechBubbleEnabled.Value then
-				DisableSpeechBubble(true);
-			else
-				EnableSpeechBubble(true);
-			end;
-		end);
-			
-		SettingsFrame.ClickEnabled.MouseButton1Click:Connect(function()
-			if CurrentDialogueContainer.Settings.ClickEnabled.Value then
-				DisableClick(true);
-			else
-				EnableClick(true);
-			end;
-		end);
-			
-		local function ClosePartSelectionFrame()
-			
-			-- Disconnect the events to make sure the user can't click them
-			Events.PartSelectionConfirmButton:Disconnect();
-			Events.PartSelectionBackButton:Disconnect();
-			Events.GetSelectedPart:Disconnect();
-			
-			-- Reset the part selection GUI
-			PartSelectionFrame.Visible = false;
-			PartSelectionFrame.SelectAPart.Text = "Select a part";
-			
-			-- Allow the player to press buttons in the settings menu
-			Busy = false;
-			
-		end;
-		
-		local function OpenPartSelectionFrame(selectingFor)
-			
-			-- Debounce
-			if Busy then
-				return;
-			end;
-			Busy = true;
-			
-			-- Change the tip based on what we're selecting
-			if selectingFor == "ClickDetector" then
-				PartSelectionFrame.Tip.Text = "Tip: You can select a ClickDetector from your object explorer!"
-			else
-				PartSelectionFrame.Tip.Text = "Tip: You can select a part from the Workspace by pressing Alt while clicking the part you want to select!";
-			end;
-			
-			-- Make the part selection frame visible
-			PartSelectionFrame.Visible = true;
-			
-			-- Let's listen when the user selects another object
-			local PartSelected = false;
-			Events.GetSelectedPart = Selection.SelectionChanged:Connect(function()
-				
-				local CurrentSelection = Selection:Get();
-				
-				-- Check if we're selecting a ClickDetector or part
-				if selectingFor == "ClickDetector" then
-					
-					-- Check if the selection is a ClickDetector
-					if #CurrentSelection ~= 1 or not CurrentSelection[1] or not CurrentSelection[1]:IsA("ClickDetector") then
-						PartSelected = false;
-						PartSelectionFrame.SelectAPart.Text = "Select a ClickDetector";
-						return;
-					end;
-					
-				else
-					
-					-- Check if the selection is a part
-					if #CurrentSelection ~= 1 or not CurrentSelection[1] or not CurrentSelection[1]:IsA("BasePart") then
-						PartSelected = false;
-						PartSelectionFrame.SelectAPart.Text = "Select a part";
-						return;
-					end;
-					
-				end;
-				
-				-- Show the user the name of the part they're currently selecting
-				PartSelectionFrame.SelectAPart.Text = CurrentSelection[1].Name;
-				
-				-- Allow the user to proceed
-				PartSelected = CurrentSelection[1];
-				
-			end);
-				
-			-- Let's listen for the user's button choices
-			Events.PartSelectionBackButton = PartSelectionFrame.BackButton.MouseButton1Click:Connect(function()
-				
-				ClosePartSelectionFrame();
-				
-			end);
-			
-			Events.PartSelectionConfirmButton = PartSelectionFrame.ConfirmButton.MouseButton1Click:Connect(function()
-				
-				-- Make sure a part is selected
-				if not PartSelected then
-					return;
-				end;
-				
-				-- Close the part selection frame
-				ClosePartSelectionFrame();
-				
-				-- Check i
-				if selectingFor == "PromptRegionPart" then
-					
-					-- Sync the part with the prompt region
-					CurrentDialogueContainer.Settings.PromptRegionPart.Value = PartSelected;
-					SettingsFrame.DefinePromptRegion.Text = CurrentDialogueContainer.Settings.PromptRegionPart.Value.Name;
-				
-				elseif selectingFor == "SpeechBubblePart" then
-					
-					CurrentDialogueContainer.Settings.SpeechBubblePart.Value = PartSelected;
-					SettingsFrame.DefineSpeechBubblePart.Text = CurrentDialogueContainer.Settings.SpeechBubblePart.Value.Name;
-					
-				elseif selectingFor == "ClickDetector" then
-					
-					CurrentDialogueContainer.Settings.ClickDetectorLocation.Value = PartSelected;
-					SettingsFrame.DefineClickDetector.Text = CurrentDialogueContainer.Settings.ClickDetectorLocation.Value.Name;
-					
-				end;
-				
-			end);
-			
-		end;
-	
-		-- Now for the part-defining buttons
-		Events.DefinePromptRegion = SettingsFrame.DefinePromptRegion.MouseButton1Click:Connect(function()
-			
-			OpenPartSelectionFrame("PromptRegionPart");
-			
-		end);
-			
-		Events.DefineSpeechBubblePart = SettingsFrame.DefineSpeechBubblePart.MouseButton1Click:Connect(function()
-			
-			OpenPartSelectionFrame("SpeechBubblePart");
-			
-		end);
-			
-		Events.DefineClickDetector = SettingsFrame.DefineClickDetector.MouseButton1Click:Connect(function()
-			
-			OpenPartSelectionFrame("ClickDetector");
-			
-		end);
-			
-		Events.DefineTimeout = SettingsFrame.TimeoutInSeconds.InputEnded:Connect(function(enterPressed)
-			
-			-- Make sure the user pressed enter
-			if enterPressed and tonumber(SettingsFrame.TimeoutInSeconds.Text) then
-				
-				-- Make sure the user enabled timeouts
-				if CurrentDialogueContainer.Settings.TimeoutEnabled.Value then
-					
-					CurrentDialogueContainer.Settings.TimeoutInSeconds.Value = SettingsFrame.TimeoutInSeconds.Text;
-					SettingsFrame.TimeoutInSeconds.PlaceholderText = CurrentDialogueContainer.Settings.TimeoutInSeconds.Value.." seconds";
-					SettingsFrame.TimeoutInSeconds.Text = "";
-					
-				end;
-				
-			end;
-			
-		end);
-		
-		-- Add the option to go back to the dialogue manager
-		Events.BackButton = SettingsFrame.BackButton.MouseButton1Click:Connect(function()
-			
-			-- Disconnect button events
-			Events.BackButton:Disconnect();
-			
-			-- Go back to the dialogue editor
-			SettingsFrame.Visible = false;
-			SettingsOpen = false;
-			
-		end);
-		
-		-- Show the settings frame
-		SettingsFrame.Visible = true;
+		plugin:OpenScript(CurrentDialogueContainer:FindFirstChild("Settings"));
 		
 	end);
 	
@@ -1125,6 +799,7 @@ local function OpenDialogueEditor()
 	end);
 	
 	-- Let's get the current dialogue settings
+	RepairNPC();
 	SyncDialogueGui(CurrentDialogueContainer["1"].Dialogue)
 	
 	DialogueMakerFrame.ViewStatus.ModelLocationFrame.ModelLocation.Text = Model.Name;
@@ -1176,85 +851,7 @@ EditDialogueButton.Click:Connect(function()
 	end;
 	
 	-- Check if there is a dialogue folder in the NPC
-	if not Model:FindFirstChild("DialogueContainer") then
-		
-		print("[Dialogue Maker] There is no DialogueContainer in "..Model.Name..". Creating one now...");
-		
-		-- Add the dialogue container to the NPC
-		local DialogueFolder = Instance.new("Folder");
-		DialogueFolder.Name = "DialogueContainer";
-		
-		-- Add configuration to the container
-		local DialogueSettings = Instance.new("Configuration");
-		DialogueSettings.Name = "Settings";
-		DialogueSettings.Parent = DialogueFolder;
-		
-		-- Add flags to the configuration
-		local PromptRegionEnabled = Instance.new("BoolValue");
-		PromptRegionEnabled.Name = "PromptRegionEnabled";
-		PromptRegionEnabled.Parent = DialogueSettings;
-		
-		local PromptRadius = Instance.new("ObjectValue");
-		PromptRadius.Name = "PromptRegionPart";
-		PromptRadius.Parent = DialogueSettings;
-		
-		local TimeoutEnabled = Instance.new("BoolValue");
-		TimeoutEnabled.Name = "TimeoutEnabled";
-		TimeoutEnabled.Parent = DialogueSettings;
-		
-		local Timeout = Instance.new("IntValue");
-		Timeout.Name = "TimeoutInSeconds";
-		Timeout.Parent = DialogueSettings;
-		
-		local SpeechBubbleEnabled = Instance.new("BoolValue");
-		SpeechBubbleEnabled.Name = "SpeechBubbleEnabled";
-		SpeechBubbleEnabled.Parent = DialogueSettings;
-		
-		local SpeechBubblePart = Instance.new("ObjectValue");
-		SpeechBubblePart.Name = "SpeechBubblePart";
-		SpeechBubblePart.Parent = DialogueSettings;
-		
-		local ClickEnabled = Instance.new("BoolValue");
-		ClickEnabled.Name = "ClickEnabled";
-		ClickEnabled.Parent = DialogueSettings;
-		
-		local ClickDetectorLocation = Instance.new("ObjectValue");
-		ClickDetectorLocation.Name = "ClickDetectorLocation";
-		ClickDetectorLocation.Parent = DialogueSettings;
-		
-		local Theme = Instance.new("StringValue");
-		Theme.Name = "Theme";
-		Theme.Parent = DialogueSettings;
-		
-		-- Create a root folder
-		local TempRootFolder = Instance.new("Folder");
-		TempRootFolder.Name = "1";
-		TempRootFolder.Parent = DialogueFolder;
-				
-		-- Create a folder to hold dialogue
-		local Dialogue = Instance.new("Folder");
-		Dialogue.Name = "Dialogue";
-		Dialogue.Parent = TempRootFolder;
-		
-		-- Create a folder to hold responses
-		local Responses = Instance.new("Folder");
-		Responses.Name = "Responses";
-		Responses.Parent = TempRootFolder;
-		
-		-- Create a folder to hold redirects
-		local Redirects = Instance.new("Folder");
-		Redirects.Name = "Redirects";
-		Redirects.Parent = TempRootFolder;
-		
-		-- Add the dialogue folder to the model
-		DialogueFolder.Parent = Model;
-		
-		print("[Dialogue Maker] Created a DialogueContainer inside of "..Model.Name..".");
-		
-	end;
-	
-	-- Set the dialogue container
-	CurrentDialogueContainer = Model.DialogueContainer;
+	RepairNPC(true);
 	
 	-- Add the chat receiver script in the starter player scripts
 	if not StarterPlayerScripts:FindFirstChild("DialogueClientScript") then
