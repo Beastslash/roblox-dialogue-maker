709ccd11235976314944e008d7283969eb2a2722
Christian <37517470+Draguwro@users.noreply.github.com>
2020-08-10 15:25:34 -0400

    Remove Before Actions for redirects and responses
    Closes #19

--- client/DialogueClientScript.lua
+++ client/DialogueClientScript.lua
@@ -5,7 +5,7 @@ local ControllerService = game:GetService("ControllerService");
 
 local Player = Players.LocalPlayer;
 local PlayerGui = Player:WaitForChild("PlayerGui");
-local RemoteConnections = ReplicatedStorage:WaitForChild("DialogueMakerRemoteConnections",3);
+local RemoteConnections = ReplicatedStorage:WaitForChild("DialogueMakerRemoteConnections", 3);
 
 -- Check if the DialogueMakerRemoteConnections folder was moved
 if not RemoteConnections then
@@ -55,22 +55,20 @@ local function ReadDialogue(npc, dialogueSettings)
 		
 		CurrentDirectory = API.Dialogue.GoToDirectory(RootDirectory, DialoguePriority:split("."));
 		
-		if CurrentDirectory.Redirect.Value and RemoteConnections.PlayerPassesCondition:InvokeServer(npc,CurrentDirectory) then
-			
-			RemoteConnections.ExecuteAction:InvokeServer(npc,CurrentDirectory,"Before");
+		if CurrentDirectory.Redirect.Value and RemoteConnections.PlayerPassesCondition:InvokeServer(npc, CurrentDirectory) then
 			
 			local DialoguePriorityPath = CurrentDirectory.RedirectPriority.Value:split(".");
 			table.remove(DialoguePriorityPath,1);
 			DialoguePriority = table.concat(DialoguePriorityPath,".");
 			CurrentDirectory = RootDirectory;
 			
-			RemoteConnections.ExecuteAction:InvokeServer(npc,CurrentDirectory,"After");
+			RemoteConnections.ExecuteAction:InvokeServer(npc, CurrentDirectory,"After");
 			
 		elseif RemoteConnections.PlayerPassesCondition:InvokeServer(npc, CurrentDirectory) then
 			
 			-- Run the before action if there is one
 			if CurrentDirectory.HasBeforeAction.Value then
-				RemoteConnections.ExecuteAction:InvokeServer(npc,CurrentDirectory,"Before");
+				RemoteConnections.ExecuteAction:InvokeServer(npc, CurrentDirectory, "Before");
 			end;
 			
 			-- Check if the message has any variables
@@ -163,12 +161,13 @@ local function ReadDialogue(npc, dialogueSettings)
 				end;
 				
 				if DividedText[index+1] and NPCTalking then
+					ThemeDialogueContainer.ClickToContinue.Visible = true;
 					NPCPaused = true;
 					
 					while NPCPaused and NPCTalking do 
 						game:GetService("RunService").Heartbeat:Wait() 
 					end;
-					
+					ThemeDialogueContainer.ClickToContinue.Visible = false;
 					NPCPaused = false;
 					Skipped = false;
 				end;
@@ -228,7 +227,7 @@ local function ReadDialogue(npc, dialogueSettings)
 			
 			-- Run after action
 			if CurrentDirectory.HasAfterAction.Value and PlayerTalkingWithNPC then
-				RemoteConnections.ExecuteAction:InvokeServer(npc,CurrentDirectory,"After");
+				RemoteConnections.ExecuteAction:InvokeServer(npc, CurrentDirectory, "After");
 			end;
 			
 			if ResponseChosen and PlayerTalkingWithNPC then
