eab0621efa585fc42be8281a7a8a431e20f431d2
Christian Toney <christian@makuwro.com>
2023-07-17 20:00:06 -0400

    Update GUI.lua

--- client/DialogueClientScript/API/GUI.lua
+++ client/DialogueClientScript/API/GUI.lua
@@ -13,41 +13,41 @@ local DefaultThemes: {
     MinimumViewportHeight: number;
     ThemeName: string;
   }  
-}? = nil;
-function GUIModule.GetDefaultThemeName(viewportWidth: number, viewportHeight: number): string
+};
+function GUIModule.getDefaultThemeName(viewportWidth: number, viewportHeight: number): string
 
   -- Check if the theme is in the cache
   if not DefaultThemes then
-    
+
     DefaultThemes = RemoteConnections.GetDefaultThemes:InvokeServer();
-    
+
   end;
   assert(DefaultThemes, "[Dialogue Maker] Couldn't get default themes from the server.");
 
   local DefaultThemeName;
   for _, themeInfo in ipairs(DefaultThemes) do
-    
+
     if viewportWidth >= themeInfo.MinimumViewportWidth and viewportHeight >= themeInfo.MinimumViewportHeight then
-      
+
       DefaultThemeName = themeInfo.ThemeName;
-      
+
     end
-    
+
   end
-  
+
   return DefaultThemeName;
 
 end;
 
-function GUIModule.CreateNewDialogueGui(theme: string?): ScreenGui
-  
+function GUIModule.createNewDialogueGui(theme: string?): ScreenGui
+
   -- Check if we have the theme
   local ThemeFolder = script.Parent.Parent.Themes;
   local DialogueGui = ThemeFolder:FindFirstChild(theme);
   if theme and not DialogueGui then
-    
+
     if theme ~= "" then
-      
+
       warn("[Dialogue Maker]: Can't find theme \"" .. theme .. "\" in the Themes folder of the DialogueClientScript. Using default theme...");
 
     end
@@ -55,27 +55,27 @@ function GUIModule.CreateNewDialogueGui(theme: string?): ScreenGui
     local ScreenGuiTest = Instance.new("ScreenGui");
     ScreenGuiTest.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui");
     local ViewportSize = ScreenGuiTest.AbsoluteSize;
-    local DefaultThemeName = GUIModule.GetDefaultThemeName(ViewportSize.X, ViewportSize.Y);
+    local DefaultThemeName = GUIModule.getDefaultThemeName(ViewportSize.X, ViewportSize.Y);
     ScreenGuiTest:Destroy();
     DialogueGui = ThemeFolder:FindFirstChild(DefaultThemeName);
-    
+
   end
-  
+
   if not DialogueGui then
 
     error("[Dialogue Maker]: There isn't a default theme", 0);
 
   end
-  
+
   -- Return the theme
   return DialogueGui:Clone();
 
 end;
 
 ReplicatedStorage:WaitForChild("DialogueMakerRemoteConnections").ChangeTheme.OnClientInvoke = function(themeName)
-  
-  script.CurrentTheme.Value = GUIModule.CreateNewDialogueGui(themeName);
-  
+
+  script.CurrentTheme.Value = GUIModule.createNewDialogueGui(themeName);
+
 end;
 
 return GUIModule;
