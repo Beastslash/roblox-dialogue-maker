071ba7b214f8f4cb57fdea69e8cfd7789a5f5d9d
Christian Toney <christian@makuwro.com>
2023-07-17 20:03:08 -0400

    Update ConditionTemplate.lua

--- server/DialogueServerScript/ConditionTemplate.lua
+++ server/DialogueServerScript/ConditionTemplate.lua
@@ -1,4 +1,5 @@
-return function(player)
+--!strict
+return function(player: Player): boolean
 	
 	-- In order for this condition to pass, it must return true.
 	-- Otherwise, lower priority dialogue/responses or no dialogue/responses will be used.
