2a42d4ca02d5523218938b338b3b917680c4f568
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-07-24 13:10:23 -0400

    simplify

--- src/DialoguePluginScript/ActionTemplate.lua
+++ src/DialoguePluginScript/ActionTemplate.lua
@@ -1,20 +1,6 @@
 --!strict
-local Action = {
+return function(Player: Player): ()
   
   -- This is the code that's ran when the action is called.
-  Execute = function(player: Player)
-    
-    
-    
-  end;
   
-  -- Stops the player from proceeding to the next dialogue/leaving
-  -- the conversation until the action is complete.
-  -- Note: If this is an "action before" function,
-  -- this causes the dialogue box to be *blank*
-  -- until the action is complete.
-  Synchronous = false; 
-  
-};
-					
-return Action;
+end;
