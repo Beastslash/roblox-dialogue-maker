88d8551e84841fef6c2c7e593b46ce20b655f494
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-07-29 19:44:56 -0400

    update a few things

--- src/DialoguePluginScript/DialogueClientScript/Types.lua
+++ src/DialoguePluginScript/DialogueClientScript/Types.lua
@@ -1,18 +1,107 @@
--- Internal effect types
+export type ContentArray = {[number]: string | Effect};
 
 export type Effect = {
 
-  -- The effect name.
-  name: string;
-
-  -- The function that runs when the effect is called.
-  run: (...any) -> (TextLabel | any);
+  run: (isPlayerSkipping: boolean) -> any;
+  
+  runFromGetPages: () -> any;
+  
+  onSkip: () -> any;
 
 }
 
-export type UseEffectFunction = ({
+export type NPCSettings = {
+
+  general: {
+
+    npcName: string; 
+
+    showName: boolean; 
+
+    fitName: boolean; 
+
+    textBoundsOffset: number; 
+
+    themeName: string; 
+
+    letterDelay: number; 
+
+    allowPlayerToSkipDelay: boolean; 
+    
+    freezePlayer: boolean; 
+
+    endConversationIfOutOfDistance: boolean;
+
+    maxConversationDistance: number;
+    
+    npcLooksAtPlayerDuringDialogue: boolean;
+    
+    npcNeckRotationMaxX: number;
+
+    npcNeckRotationMaxY: number;
+    
+    npcNeckRotationMaxZ: number;
+    
+  };
+
+  promptRegion: {
+
+    enabled: boolean;
+    
+    location: BasePart?;
+    
+  };
+
+  timeout: {
+
+    enabled: boolean;
+    
+    seconds: number;
+    
+    waitForResponse: boolean;
+    
+  };
+
+  speechBubble: {
+
+    enabled: boolean;
+    
+    location: BasePart?;
+    
+  };
+
+  clickDetector: {
+
+    enabled: boolean;
+    
+    autoCreate: boolean;
+    
+    disappearsWhenDialogueActive: boolean;
+    
+    location: ClickDetector?;
+
+  };
+
+  proximityPrompt: {
+
+    enabled: boolean;
+    
+    autoCreate: boolean;
+    
+    location: ProximityPrompt?;
+    
+  };
+
+};
+
+export type UseEffectProperties = {
+
   name: string;
+
   [string]: any;  
-}) -> Effect;
+
+};
+
+export type UseEffectFunction = (effectProperties: UseEffectProperties) -> Effect;
 
 return {};
